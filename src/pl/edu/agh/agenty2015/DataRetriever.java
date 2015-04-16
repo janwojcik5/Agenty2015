fd1af77d1d778b89f3f064000f327d2cff37644e
oem <oem@PC>
2015-04-16 14:10:04 +0200

    Created a stub of a DataRetriever class.

---
+++ src/pl/edu/agh/agenty2015/DataRetriever.java
@@ -0,0 +1,17 @@
+package pl.edu.agh.agenty2015;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+/**
+ * Created by oem on 2015-04-16.
+ */
+public class DataRetriever {
+
+    private String baseURL;
+
+    public DataRetriever(String baseURL) {
+
+    }
+
+
+}
